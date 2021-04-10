939b7d48bfdcf76143567ea68d2115ad81f4c5e6
skygod <skygod1994323@gmail.com>
2021-04-10 16:04:50 -0700

    a

--- api.php
+++ api.php
@@ -1,31 +1,34 @@
 <?php 
-    // $host = "{imap.yahoo.co.uk:993/imap/ssl}INBOX";
-    // $username = "kusnohadie-hangman";
-    // $password = "787DrmLnr1708241092";
+    $host = "{imap.mail.ru:993/imap/ssl/novalidate-cert}";
+    $username = "maksim.matveev98@mail.ru";
+    $password = "ryddlf199082";
 
-    // $inbox = imap_open($host,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
+    $inbox = imap_open($host,$username,$password, NULL, 1, array('DISABLE_AUTHENTICATOR' => 'GSSAPI')) or die('Cannot connect: ' . imap_last_error());
 
-    // var_dump($inbox);
-    $imap_server = $_POST['imap_server'];
-    $host = "{" . $imap_server . ":993/imap/ssl/novalidate-cert}";
-    $username = $_POST['imap_server'];
-    $password = $_POST['imap_server'];
+    var_dump($inbox);
+	
+	imap_close($inbox);
 
-    try {
-        $inbox = imap_open($host,$username,$password);
-        if ($inbox) {
-            echo json_encode(array(
-                'success' => true
-            ));
-        } else {
-            echo json_encode(array(
-                'success' => false
-            ));
-        }
+    // $imap_server = $_POST['imap_server'];
+    // $host = "{" . $imap_server . ":993/imap/ssl/novalidate-cert}";
+    // $username = $_POST['imap_server'];
+    // $password = $_POST['imap_server'];
+
+    // try {
+    //     $inbox = imap_open($host,$username,$password);
+    //     if ($inbox) {
+    //         echo json_encode(array(
+    //             'success' => true
+    //         ));
+    //     } else {
+    //         echo json_encode(array(
+    //             'success' => false
+    //         ));
+    //     }
         
-    } catch (Exception $e) {
-        echo json_encode(array(
-            'success' => false
-        ));
-    }
+    // } catch (Exception $e) {
+    //     echo json_encode(array(
+    //         'success' => false
+    //     ));
+    // }
 ?>
