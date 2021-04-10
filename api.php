ad713453ab8f823d727b09e5f0081f75aaa2dcbf
skygod <skygod1994323@gmail.com>
2021-04-10 16:06:35 -0700

    a

--- api.php
+++ api.php
@@ -1,34 +1,24 @@
 <?php 
-    $host = "{imap.mail.ru:993/imap/ssl/novalidate-cert}";
-    $username = "maksim.matveev98@mail.ru";
-    $password = "ryddlf199082";
+    $imap_server = $_POST['imap_server'];
+    $host = "{" . $imap_server . ":993/imap/ssl/novalidate-cert}";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    $inbox = imap_open($host,$username,$password, NULL, 1, array('DISABLE_AUTHENTICATOR' => 'GSSAPI')) or die('Cannot connect: ' . imap_last_error());
-
-    var_dump($inbox);
-	
-	imap_close($inbox);
-
-    // $imap_server = $_POST['imap_server'];
-    // $host = "{" . $imap_server . ":993/imap/ssl/novalidate-cert}";
-    // $username = $_POST['imap_server'];
-    // $password = $_POST['imap_server'];
-
-    // try {
-    //     $inbox = imap_open($host,$username,$password);
-    //     if ($inbox) {
-    //         echo json_encode(array(
-    //             'success' => true
-    //         ));
-    //     } else {
-    //         echo json_encode(array(
-    //             'success' => false
-    //         ));
-    //     }
+    try {
+        $inbox = imap_open($host,$username,$password);
+        if ($inbox) {
+            echo json_encode(array(
+                'success' => true
+            ));
+        } else {
+            echo json_encode(array(
+                'success' => false
+            ));
+        }
         
-    // } catch (Exception $e) {
-    //     echo json_encode(array(
-    //         'success' => false
-    //     ));
-    // }
+    } catch (Exception $e) {
+        echo json_encode(array(
+            'success' => false
+        ));
+    }
 ?>
